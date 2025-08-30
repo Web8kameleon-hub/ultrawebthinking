4d34b6a40a6592f3424f303031ab545b02aa32eb
Ledjan Ahmati <jonadeals@gmail.com>
2025-08-30 13:41:59 +0200

    fix: align Web8TabSystem imports; add NeuralSearch + stubs; add AGICore/SearchInterface

--- .next/server/middleware-build-manifest.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:92cb97140fc286815cd0ea370c254da32c25e23f0dcac102c33e4a09f16ac15f
-size 823
--- components/Web8TabSystem.tsx
+++ components/Web8TabSystem.tsx
@@ -13,9 +13,9 @@
 
 import React, { useState, useEffect, useCallback } from 'react'
 import { motion } from 'framer-motion'
-import { AGICore } from '../frontend/components/AGICore'
-import { NeuralAnalytics } from '../frontend/components/NeuralAnalytics'
-import { NeuralSearch } from '../frontend/components/NeuralSearch'
+import { AGICore } from './pages/AGICore'
+import { NeuralAnalytics } from './NeuralAnalytics'
+import { NeuralSearch } from './NeuralSearch'
 import { AGITabSystem } from './AGITabSystem'
 import { useAGIRealTime } from '../hooks/useAGIRealTime'
 import AGIControlCenter from './AGIControlCenter'
