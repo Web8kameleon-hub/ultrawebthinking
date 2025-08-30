c2ac3e3d293dde98dff6c0928d4ab41a6416e180
Ledjan Ahmati <jonadeals@gmail.com>
2025-08-30 13:45:02 +0200

    chore: add stubs for dynamic components used in Web8TabSystem

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1033ed957d998c4a8a7022ec5d39cf9373d93a07788ee0edfb038e21f9027dc
-size 1565
+oid sha256:43d608b8558885475aaf64c8ce6e3cf39a870953845bacfdf6da56dc5e0e0254
+size 8668
