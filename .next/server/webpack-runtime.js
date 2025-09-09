c4883f422d7b74f59f87f1f395abd5ea7171533b
UltraWebThinking <ultrawebthinking@example.com>
2025-09-09 22:38:15 +0200

    chore: remove .next and npm lockfile

--- .next/server/webpack-runtime.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:45fad2718cc8d10c479a21d44363ba5c25687f9043ae8646457b37c3644229cf
-size 8821
--- .next/types/app/layout.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1cc965ab7966d27349c710f894d54137d796433a7d7cc423e8936e18717a51bc
-size 3480
