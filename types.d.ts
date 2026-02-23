a485c732969b5a3df4d16449ee22d8eb7bb6b892
ledjanahmati <amati.bau@gmail.com>
2026-02-23 18:21:14 +0100

    fix: add hot-formula-parser type declaration

--- types.d.ts
+++ types.d.ts
@@ -21,4 +21,15 @@ declare module 'react-dom' {
   export as namespace ReactDOM;
 }
 
+// Missing module declarations
+declare module 'hot-formula-parser' {
+  export class Parser {
+    parse(expression: string): { error: string | null; result: any };
+    setFunction(name: string, fn: (...args: any[]) => any): void;
+    setVariable(name: string, value: any): void;
+    on(event: string, callback: (...args: any[]) => void): void;
+    supported(thing: string): boolean;
+  }
+}
+
 export {};
